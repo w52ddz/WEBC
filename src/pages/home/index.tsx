230d427f38bd52a40d00bd7a67ae202868102b3d
wylong <wylong.ok@163.com>
2023-07-31 19:51:21 +0800

    video aspect rate

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 // import { useNavigate } from 'react-router-dom'
 // import store from '@/redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 // import { useSelector } from 'react-redux'
 import { Carousel } from 'antd'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
@@ -14,7 +14,7 @@ function Layout() {
   // const navigate = useNavigate()
   const videoRef = React.useRef<HTMLVideoElement>(null)
   // const count = useSelector((state: any) => state.countReducer.count)
-
+  const [videoStyles, setVideoStyles] = useState<React.CSSProperties>({})
   // const [name, setName] = useState<string>(store.getState().nameReducer.name) //获取store.name作为默认值
   // store.subscribe(() => {
   //   const store_data = store.getState()
@@ -31,23 +31,38 @@ function Layout() {
     { type: 'video', src: video },
   ]
 
+  const handleVideoStyleWhenResize = () => {
+    const { innerWidth, innerHeight } = window
+    if (innerWidth / innerHeight >= 1.9) {
+      setVideoStyles({
+        width: innerWidth + 'px',
+        height: 'auto',
+      })
+    } else {
+      setVideoStyles({
+        width: 'auto',
+        height: innerHeight + 'px',
+      })
+    }
+  }
+
   useEffect(() => {
     // dom mouted，save video element and pause
     videoEl = videoRef.current
     videoEl!.currentTime = 0
     videoEl!.pause()
-  })
+    // 根据当前窗口宽高设置video样式
+    handleVideoStyleWhenResize()
+    window.addEventListener('resize', handleVideoStyleWhenResize, false)
+    return () => {
+      window.removeEventListener('resize', handleVideoStyleWhenResize, false)
+    }
+  }, [])
 
   const generateCarouselList = (): JSX.Element[] => {
     return carouselDataList.map((item, index) => {
       const itemStyle: React.CSSProperties = {}
       let innerEl = <></>
-      const videoStyle: React.CSSProperties = {}
-      const isMobile = judgeMobile()
-      if (isMobile) {
-        videoStyle.width = 'auto'
-        videoStyle.height = '100%'
-      }
       // 背景图
       if (item.type === 'img') {
         itemStyle.backgroundImage = `url(${item.src})`
@@ -60,7 +75,7 @@ function Layout() {
             ref={videoRef}
             className='carousel-video'
             src={video}
-            style={videoStyle}
+            style={videoStyles}
           ></video>
         )
       }
